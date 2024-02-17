7e1d6b98ef910d9732d126c992fbc436eeaa7983
Dániel Novák <novak@xund.ai>
2024-02-17 15:19:23 +0100

    Add app folder structure

--- Coins/Coins/Presentation/Overview/OverviewView.swift
+++ Coins/Coins/Presentation/Overview/OverviewView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  OverviewView.swift
 //  Coins
 //
 //  Created by Dániel Novák on 17/02/2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OverviewView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    OverviewView()
 }
