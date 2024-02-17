b561045c6bc891c6ae543cd7ddb2bfd382a80bb1
Dániel Novák <novak@xund.ai>
2024-02-17 23:35:55 +0100

    Add GetCurrenciesUseCase and GetCurrenciesUseCaseUseCaseImplementation

--- Coins/Coins/Core/Error/ApiError.swift
+++ Coins/Coins/Core/Error/ApiError.swift
@@ -12,3 +12,23 @@ enum ApiError: Error {
     case request
     case unknown
 }
+
+extension ApiError {
+    private enum Constants {
+        static let configurationErrorMessage = "Configuration error"
+        static let decodingErrorMessage = "Decoding error"
+        static let noConnectionErrorMessage = "No connection"
+        static let requestErrorMessage = "Request failed"
+        static let unknownErrorMessage = "Unknown error"
+    }
+    
+    var message: String {
+        switch self {
+        case .configuration: return Constants.configurationErrorMessage
+        case .decoding: return Constants.decodingErrorMessage
+        case .noConnection: return Constants.noConnectionErrorMessage
+        case .request: return Constants.requestErrorMessage
+        case .unknown: return Constants.unknownErrorMessage
+        }
+    }
+}
---
+++ Coins/Coins/Domain/UseCase/GetCurrenciesUseCase.swift
@@ -0,0 +1,22 @@
+//
+//  GetCurrenciesUseCase.swift
+//  Coins
+//
+//  Created by Dániel Novák on 17/02/2024.
+//
+
+protocol GetCurrenciesUseCase {
+    func getCurrencies() async throws -> [CurrencyEntity]
+}
+
+class GetCurrenciesUseCaseUseCaseImplementation: GetCurrenciesUseCase {
+    private let repository: Repository
+    
+    init(repository: Repository) {
+        self.repository = repository
+    }
+    
+    func getCurrencies() async throws -> [CurrencyEntity] {
+        try await repository.getCurrencies()
+    }
+}
